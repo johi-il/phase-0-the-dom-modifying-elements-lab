78428f0fcf1ce683a024bb2dc90ed7cc432db65f
lewis wanjohi <i.johilewis@gmail.com>
2025-09-16 12:04:50 +0300

    DOM done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const newHeader = document.createElement("h1");
+newHeader.textContent = "lewis is the champion";
+newHeader.setAttribute("id","victory")
+
+document.body.appendChild(newHeader)
+
+
+
+
+
+
